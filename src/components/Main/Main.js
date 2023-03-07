f6eec6d1f5e478387459ab05af949bc31a33ebb3
Андрей Гришков <andreygorm@gmail.com>
2023-03-07 23:09:30 +0300

    feat: main component add

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -12,10 +12,23 @@ function Main() {
           <p className="main__heart-text">С восьмым
             <br></br>марта!!!</p>
         </div>
+        <div className="main__img-eight-march"></div>
         <div className='main__text-container'>
-          <h3>Любимая Светочка</h3>
-          <p>С восьмым марта поздравляю<br></br>
+          <h3 className='main__title'>Любимая Светочка</h3>
+          <p className="main__text">С восьмым марта поздравляю<br></br>
             Шлю тебе любовь из далека <br></br>
+            И всем сердцем я желаю <br></br>
+            Чтобы ты была со мной всегда
+          </p>
+          <p className="main__text">Ты одна там не скучай<br></br>
+            Позвони подруге, сына пригласи <br></br>
+            по Гешпански книгу почитай <br></br>
+            Марсу кушать положи
+          </p>
+          <p className="main__text">Я люблю тебя Цветочек<br></br>
+            Но не долго мне страдать<br></br>
+            Будет котик и грибочек<br></br>
+            С миллионом роз встречать!
           </p>
 
         </div>
